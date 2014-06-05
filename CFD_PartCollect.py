c5b70c78d0a40ca77a265d566a44014de565c60e
XNShen <shen_xn@163.com>
2014-06-05 09:04:13 +0800

    Test draft 2

--- CFD_PartCollect.py
+++ CFD_PartCollect.py
@@ -55,9 +55,10 @@ def readExcel(fileName, tag):
     book = open_workbook(fileName)
     sheet = book.sheet_by_index(sheetIndex)
    
-    dataTotal = []
+    dataTotal = []          # store the data of the entire sheet
+    
     for colNum in range(sheet.ncols):
-        dataGroup = []
+        dataGroup = []      # store the data of each column
         dataTemp = []
             
         if (sheet.cell_value(0, colNum) != ''):
@@ -78,25 +79,28 @@ def readExcel(fileName, tag):
                     (tag.search(col_values[ind + counter]) == None)):                        
                         chunk.append(col_values[ind + counter])
                         counter += 1
-                        
+                    
+                    # extract the data and row name                       
                     dataSeg, rowName = chunk_process(chunk)
-                        
-                     
-                    if dataGroup == []:
-                        for i in range(4):
+                    
+                    # initiate the entire data array if not exist yet.                        
+                    if len(dataGroup) == 0:
+                        for i in range(4): # "Escaped", "Trapped", "Incomplete", "Net"
                             dataGroup.append(pd.DataFrame(dataSeg[:,i], index=rowName, \
                                                 columns=[sheet.cell_value(0, colNum)]))
+                    # if the final result array exists, merge with the current new data.
                     else:
                         for i in range(4):
                             dataTemp.append(pd.DataFrame(dataSeg[:,i], index=rowName, \
-                                                columns=[sheet.cell_value(0, colNum)]))     
+                                                columns=[sheet.cell_value(0, colNum)]))
                             dataGroup[i] = pd.concat([dataGroup[i], dataTemp[i]], axis=0, join='inner')
+                        # clear dataTemp for next time.
                         dataTemp = []                         
-                                    
-                        ind += counter
+                    # jump over the analyzed chunk.
+                    ind += counter
     
             dataTotal.append(dataGroup) 
-                        # ALl chunked at each col is loaded to the memory   
+            # ALl chunked at each col is loaded to the memory   
     return dataTotal
 
 def chunk_process(chunk):
@@ -113,17 +117,16 @@ def chunk_process(chunk):
     # characters for separating counting lines
     sepChar = '----'
     collectionStatus = ['Incomplete', 'Trapped', 'Escaped', 'Net']
-    sizeCnt = 0
+    lineOffset = 5
     # regular expression for " Trapped - Zone 22       1.042e-04  1.042e-04  0.000e+00"
     pat = r'\s+(\w+).(-.\w+\s\d+)?\s+(\d+\.\d+[e|E][+|-]\d+)\s+(\d+\.\d+[e|E][+|-]\d+)\s+(\d+\.\d+[e|E][+|-]\d+)' 
 
     # buffer results for each segment
-    dataArray = np.zeros((500, 4))
-    diaList = []    # diameter list
+    dataArray = np.zeros(4)
     
     # extract 'injection-75-141' style expression    
     # looping through the given column
-    for i in range(np.size(chunk)):
+    for i in range(len(chunk)):
         # find 'Escaped - Zone' line -- input information
         if (escaped_tag.search(chunk[i]) != None or trapped_tag.search(chunk[i])):
             # find injection information 'injection-75-142.9  198'
@@ -132,67 +135,65 @@ def chunk_process(chunk):
                 # This is the injection info section
                 # extract the numbers of the 'injection...' string
                 dia  = r_tag.findall(chunk[i])[0]
-                # forming the row name
-                diaList.append(dia)
 
         # This is the injection results section
         # starting here "Trapped", "Escaped", "Incomplete" & "Net" are analyzed
         # separately
         if (res_tag.search(chunk[i]) != None):
-        # find the 'mass transfer summary' name tagn
+        # find the 'mass transfer summary' name tag
             k = 0                       # advancing index 
-            SingleLineMarker = 0        # Possible single results
+            ResLineNum = 0              # Possible single results
             # Read each line of the summary results
             # '- - - -' is the separation line
-            while ((i+k+5) < 12 and (sepChar not in chunk[i+k+5])):  
-            # still in the detail report
-                match = re.search(pat, chunk[i+k+5])
+            # The condition is a) the search less than the total line number
+            #                  b)'----' is not in the line
+            # Bascially, search between '----' and '-----'
+            # '5' is the preset offset
+            while ((i+k+lineOffset) < len(chunk) and \
+                        (sepChar not in chunk[i+k+lineOffset])):  
+                # Search for reports on "Trapped", "Escaped", "Incomplete" & "Net" 
+                match = re.search(pat, chunk[i+k+lineOffset])
                 # extract the mass fraction
                 if match:  # regular expression matching successful
                     if match.group(1) in collectionStatus:  
                         index = collectionStatus.index(match.group(1))
-                        dataArray[sizeCnt, index] = float(match.group(3))
-                        SingleLineMarker += 1
+                        dataArray[index] = float(match.group(3))
+                        # If there is only one line, then there is no 'Net' line below                    
+                        ResLineNum += 1
                     else:
-                        print 'The initial items are unexpected.'
+                        print 'The initial items are unexpected. Change the lineOffset value.'
                         print 'The matched string is: %s' %match.group()
-                        print 'The original string is: %s' %chunk[i+k+5]                       
-
+                        print 'The original string is: %s' %chunk[i+k+lineOffset]                       
                 k = k + 1
             
-            # - - - - 
             # When '- - - -' is met, move the next line
-            if SingleLineMarker < 1:
-                k = k+1
-                match = re.search(pat,chunk[i+5+k])  
+            # If only one line is present, then no need to analyzing the following line.
+            if ResLineNum > 1:
+                k = k + 1     # jump over '---' line
+                match = re.search(pat,chunk[i+k+lineOffset])  
                 # extract the mass fraction
                 if match:  # regular expression matching successful
                     if match.group(1) in collectionStatus:  
                         index = collectionStatus.index(match.group(1))
-                        if index == 3:
-                            dataArray[sizeCnt, index] = float(match.group(3))
+                        if index == 3:          # This line has to be 'Net'
+                            dataArray[index] = float(match.group(3))
                         else:
-                            print 'Index is %d' % index
+                            print 'Index is \" %s \"' % collectionStatus[index]
                             print 'The line should start with *Net*'
-                            print 'The original string is: %s' %chunk[i+5+k]
+                            print 'The original string is: %s' %chunk[i+lineOffset+k]
                     else:
-                        print 'Regular expression confronted unexpected pattern (Reading Net)'
+                        print 'Regular expression finds items out of four choices (Reading Net)'
                         print 'The matched string is: %s' %match.group()
-                        print 'The original string is: %s' %chunk[i+5+k]                      
-                        print 'The original string is: %s' %chunk[i+5+k]                 
+                        print 'The original string is: %s' %chunk[i+lineOffset+k]                      
+                        print 'The original string is: %s' %chunk[i+lineOffset+k]                 
                 else:
-                    print 'Regular expression mis-match'
-                    print 'The original string is: %s' %chunk[i+5+k]
+                    print 'Regular expression mis-match. The *Net* line is not formatted correctly.'
+                    print 'The original string is: %s' %chunk[i+lineOffset+k]
             else:
                 # if SingLeLineMarker on, copy the previous line
-                dataArray[sizeCnt, 3] =  dataArray[sizeCnt, index]
-                    
-            sizeCnt=sizeCnt+1   # 'Mass Transfer Summary' is found 
-            i = i+1             # jump the next block
-        else:
-            i = i+1
+                dataArray[3] =  dataArray[index]
 
-    return dataArray[0:sizeCnt,:], [dia]
+    return dataArray, dia
 
 def main():
     rt = Tkinter.Tk()
